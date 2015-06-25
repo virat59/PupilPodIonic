e7dee796cc192aed563d4b308d6d5ccddef4e179
virat59 <virat59@gmail.com>
2015-06-25 12:02:01 +0530

    Commit 105
    Commit 105

--- www/app/controllers/PPODController.js
+++ www/app/controllers/PPODController.js
@@ -9,6 +9,7 @@ app.controller('PPODController',function($scope,PPODService,$http,$window,$docum
 	};
 	
 	$scope.students = {};
+	$scope.student = "";
 	
 	$scope.doRefresh = function() {
 		console.log('Refreshing!');
@@ -170,10 +171,12 @@ app.run(function($rootScope) {
 	});
 });
 
+
 app.directive("dropdown", function($rootScope,sharedProperties) {
 	return {
 		restrict: "E",
 		templateUrl: "app/directives/templates/dropdown.html",
+		transclude: true,
 		scope: {
 			placeholder: "@",
 			list: "=",
@@ -187,6 +190,7 @@ app.directive("dropdown", function($rootScope,sharedProperties) {
 			scope.select = function(item) {
 				scope.isPlaceholder = false;
 				scope.selected = item;
+				scope.listVisible = false;
 			};
 
 			scope.isSelected = function(item) {
@@ -198,11 +202,15 @@ app.directive("dropdown", function($rootScope,sharedProperties) {
 			};
 
 			$rootScope.$on("documentClicked", function(inner, target) {
-				//console.log($(target[0]).is(".dropdown-display.clicked") || $(target[0]).parents(".dropdown-display.clicked").length > 0);
-				if (!$(target[0]).is(".dropdown-display.clicked") && !$(target[0]).parents(".dropdown-display.clicked").length > 0)
-					scope.$apply(function() {
-						scope.listVisible = false;
-					});
+				/*if (!$(target[0]).is(".dropdown-display.clicked") && !$(target[0]).parents(".dropdown-display.clicked").length > 0){
+					if(!scope.$$phase) {
+						scope.$apply(function() {
+							console.log('Apply Called');
+							//scope.listVisible = false;
+						});
+					}
+					
+				}*/
 			});
 
 			scope.$watch("selected", function(value) {
@@ -214,151 +222,6 @@ app.directive("dropdown", function($rootScope,sharedProperties) {
 			});
 		}
 	}
-});
-
-app.directive('dragToDismiss', function($drag, $parse, $timeout){
-  return {
-    restrict: 'A',
-    compile: function(elem, attrs) {
-      var dismissFn = $parse(attrs.dragToDismiss);
-      return function(scope, elem, attrs){
-        var dismiss = false;
-
-        $drag.bind(elem, {
-          constraint: {
-            minX: 0, 
-            minY: 0, 
-            maxY: 0 
-          },
-          move: function(c) {
-            if( c.left >= c.width / 4) {
-              dismiss = true;
-              elem.addClass('dismiss');
-            } else {
-              dismiss = false;
-              elem.removeClass('dismiss');
-            }
-          },
-          cancel: function(){
-            elem.removeClass('dismiss');
-          },
-          end: function(c, undo, reset) {
-            if (dismiss) {
-              elem.addClass('dismitted');
-              $timeout(function() { 
-                scope.$apply(function() {
-                  dismissFn(scope);  
-                });
-              }, 400);
-            } else {
-              reset();
-            }
-          }
-        });
-      };
-    }
-  };
-});
-
-//
-// Another `$drag` usage example: this is how you could create 
-// a touch enabled "deck of cards" carousel. See `carousel.html` for markup.
-//
-app.directive('carousel', function(){
-  return {
-    restrict: 'C',
-    scope: {},
-    controller: function($scope) {
-      this.itemCount = 0;
-      this.activeItem = null;
-
-      this.addItem = function(){
-        var newId = this.itemCount++;
-        this.activeItem = this.itemCount == 1 ? newId : this.activeItem;
-        return newId;
-      };
-
-      this.next = function(){
-        this.activeItem = this.activeItem || 0;
-        this.activeItem = this.activeItem == this.itemCount - 1 ? 0 : this.activeItem + 1;
-      };
-
-      this.prev = function(){
-        this.activeItem = this.activeItem || 0;
-        this.activeItem = this.activeItem === 0 ? this.itemCount - 1 : this.activeItem - 1;
-      };
-    }
-  };
-});
-
-app.directive('carouselItem', function($drag) {
-  return {
-    restrict: 'C',
-    require: '^carousel',
-    scope: {},
-    transclude: true,
-    template: '<div class="item"><div ng-transclude></div></div>',
-    link: function(scope, elem, attrs, carousel) {
-      scope.carousel = carousel;
-      var id = carousel.addItem();
-      
-      var zIndex = function(){
-        var res = 0;
-        if (id == carousel.activeItem){
-          res = 2000;
-        } else if (carousel.activeItem < id) {
-          res = 2000 - (id - carousel.activeItem);
-        } else {
-          res = 2000 - (carousel.itemCount - 1 - carousel.activeItem + id);
-        }
-        return res;
-      };
-
-      scope.$watch(function(){
-        return carousel.activeItem;
-      }, function(n, o){
-        elem[0].style['z-index']=zIndex();
-      });
-      
-
-      $drag.bind(elem, {
-        constraint: { minY: 0, maxY: 0 },
-        adaptTransform: function(t, dx, dy, x, y, x0, y0) {
-          var maxAngle = 15;
-          var velocity = 0.02;
-          var r = t.getRotation();
-          var newRot = r + Math.round(dx * velocity);
-          newRot = Math.min(newRot, maxAngle);
-          newRot = Math.max(newRot, -maxAngle);
-          t.rotate(-r);
-          t.rotate(newRot);
-        },
-        move: function(c){
-          if(c.left >= c.width / 4 || c.left <= -(c.width / 4)) {
-            elem.addClass('dismiss');  
-          } else {
-            elem.removeClass('dismiss');  
-          }          
-        },
-        cancel: function(){
-          elem.removeClass('dismiss');
-        },
-        end: function(c, undo, reset) {
-          elem.removeClass('dismiss');
-          if(c.left >= c.width / 4) {
-            scope.$apply(function() {
-              carousel.next();
-            });
-          } else if (c.left <= -(c.width / 4)) {
-            scope.$apply(function() {
-              carousel.next();
-            });
-          }
-          reset();
-        }
-      });
-    }
-  };
 });
 
 app.controller('loginController',function($scope,PPODService,$http,$window,$document,sharedProperties,myCache,$q,$state,$ionicSideMenuDelegate,$timeout){
